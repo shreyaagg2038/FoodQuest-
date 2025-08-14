28e4a2505e047bc3748a417cb4c3b9a5c3d8f7a9
Shreya Aggarwal <shreyaagg2038@gmail.com>
2025-08-14 22:53:30 +0530

    Explored React Lifecycle methods

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,15 @@ import UserClass from "./UserClass"
 class About extends Component{
     constructor(props){
         super(props);
-        console.log("Parent Constructor")
     }
 
     componentDidMount(){
-        console.log("Parent did Mount")
     }
 
     render(){
-        console.log("Parent Render")
         return (
             <div><h1>About Us Page</h1>
-            <UserClass name="Shreya class" location="Ambala Cantt class" designation="Software Engineer class"/>
+            <UserClass/>
             </div>
         )
     }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,42 +1,47 @@
 import React from 'react';
+import Contact from './Contact'
 
 class UserClass extends React.Component{
 
     constructor(props){
         super(props);
         this.state={
-            count:0,
-            count1:1,
+            resInfo:{
+                name:"dummy",
+                location:"inida",
+            }
         }
         console.log("Child Constructor");
 
     }
 
-    componentDidMount(){
-        console.log("Child Did Mount");
-        //API calls here ?? But Why 
-        // because we want to give better user experience -> loads->renders->apicall->re-renders
+    async componentDidMount(){
+        console.log( "Child Did Mount");
+        const data = await fetch("https://api.github.com/users/shreyaagg2038");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            resInfo:json
+        })
     }
 
-    render(){
-        const {name,location,designation} = this.props;
-        const {count,count1} =this.state;
 
+    componentDidUpdate(){
+        console.log( "Child Did Update");
+    }
+
+    componentWillUnmount(){
+        console.log( "Child unmounted");     
+    }
+    
+    render(){
         console.log("Child Render");
+        const {name,location} = this.state.resInfo;
 
         return(
             <div style={{margin:"10px",padding:"10px",border:"solid black"}}>
-                <h1>Count:{count}</h1>
-                <button onClick={()=>{
-                    this.setState({count: this.state.count+1});
-                }}>Increase Count</button>
-                <h1>Count1:{count1}</h1>
-                <button onClick={()=>{
-                    this.setState({count1: this.state.count1+1});
-                }}>Increase Count</button>
                 <h1>{name}</h1>
                 <h3>{location}</h3>
-                <h3>{designation}</h3>
             </div>
         )
     }

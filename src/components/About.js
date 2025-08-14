95073f3119318d721844cf8e3a02adf57628aae8
Shreya Aggarwal <shreyaagg2038@gmail.com>
2025-08-14 20:48:21 +0530

    setState in React class based component

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@ import UserClass from "./UserClass"
 const About =()=>{
     return (
         <div><h1>About Us Page</h1>
-        <User name="Shreya" location="Ambala Cantt" designation="Software Engineer"/>
         <UserClass name="Shreya class" location="Ambala Cantt class" designation="Software Engineer class"/>
         </div>
     )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,17 +6,23 @@ class UserClass extends React.Component{
         super(props);
         this.state={
             count:0,
-            count2:0
+            count1:1,
         }
 
     }
     render(){
         const {name,location,designation} = this.props;
-        const {count,count2} =this.state;
+        const {count,count1} =this.state;
         return(
             <div style={{margin:"10px",padding:"10px",border:"solid black"}}>
                 <h1>Count:{count}</h1>
-                <h1>Count2:{count2}</h1>
+                <button onClick={()=>{
+                    this.setState({count: this.state.count+1});
+                }}>Increase Count</button>
+                <h1>Count1:{count1}</h1>
+                <button onClick={()=>{
+                    this.setState({count1: this.state.count1+1});
+                }}>Increase Count</button>
                 <h1>{name}</h1>
                 <h3>{location}</h3>
                 <h3>{designation}</h3>
